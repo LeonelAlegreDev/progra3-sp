e68eb0114d1323e47c2ff576e4a14e33153428f3
LeonelAlegreDev <78889336+LeonelAlegreDev@users.noreply.github.com>
2023-12-10 17:28:26 +0000

    Prueba del middleware AccesLogging

--- middlewares/AccessLogging.php
+++ middlewares/AccessLogging.php
@@ -15,11 +15,11 @@ class AccessLogging
             'user_agent' => $request->getHeaderLine('User-Agent')   // work
         ];
 
-        echo $data->date;
-        echo $data->method;
-        echo $data->uri;
-        echo $data->ip;
-        echo $date->user_agent;
+        echo $data["date"];
+        echo $data["method"];
+        echo $data["uri"];
+        echo $data["ip"];
+        echo $date["user_agent"];
 
 
         return $handler->handle($request);
